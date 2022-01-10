b95a72357535b606ad21d1cb8579ba078a07ab77
Harsh Agarwal <agharsh@amazon.com>
2022-01-10 14:21:16 +0530

    initial commit

---
+++ src/main/java/com/example/Spring/security/student/Student.java
@@ -0,0 +1,11 @@
+package com.example.Spring.security.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Student {
+    private final Integer id;
+    private final String name;
+}
---
+++ src/main/java/com/example/Spring/security/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.Spring.security.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+        new Student(1,  "Harsh"),
+        new Student(2, "Ryuga"),
+        new Student(3, "Vegeta")
+    );
+
+    @GetMapping(path="{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer id) {
+        return STUDENTS.stream()
+                .filter(student -> id.equals(student.getId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + id + " not found"));
+    }
+}
